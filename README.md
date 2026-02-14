f9a57e59d7a347291c3b1b6c5dfba8f38a541804
Chris Wright <cwright@richlandlibrary.com>
2026-02-14 17:21:23 -0500

    updating

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # 🖥️ Typer & FastAPI
 
 A repo of FastAPI and Typer projects for learning purposes.
+
+## Typer
+
+Current `typer_main.py` commands:
+
+- `python3 typer_main.py --help`
+- `python3 typer_main.py Chris`
+- `python3 typer_main.py Chris False Earth`
--- typer_main.py
+++ typer_main.py
@@ -1,27 +1,56 @@
 from __future__ import annotations
 
+import random
 import typer
 from rich import print
-from typing import Dict, List, Union
+from rich.console import Console
+from rich.table import Table
+from typing import Annotated, Dict, List, Union
 
 data: Dict[str, Union[str, int, List[Dict[str, str]], bool]] = {
 	"name": "Chris",
 	"age": 31,
-	"items": [{"name": "Keyboard"}, {"name": "Guitar"}],
-	"active": True,
+	"items": [{"name": "Keyboard :computer:"}, {"name": "Guitar"}],
+	"active": False,
 	"affiliation": "Communist",
 }
 
+console = Console()
+err_console = Console(stderr=True)
 app = typer.Typer()
 
 
+def get_name():
+	return random.choice(["Frodo", "Gandalf", "Aragorn"])
+
+
 @app.command()
-def main():
-	print("Here's the data")
+def main(
+	dynamic_name: Annotated[
+		str,
+		typer.Argument(
+			default_factory=get_name,
+			help="The name of the user to greet",
+			show_default="Think of something cool frfr",
+			metavar="dynamic_name",
+		),
+	],
+	good: Annotated[
+		bool,
+		typer.Argument(metavar="👍 good 👎", rich_help_panel="Secondary Arguments"),
+	] = True,
+	other_stuff: Annotated[
+		str, typer.Argument(hidden=True, envvar=["AWESOME_NAME", "GOD_NAME"])
+	] = "World",
+):
+	"""
+	Displays a dictionary, shows a table, prints to error console.
+	Says hi to NAME.
+	"""
+	# Displaying the data in the dictionary
+	print("[bold blue]Here's the data[/bold blue]:")
 	for k, v in data.items():
-		if k == "name":
-			print(f"[bold green]{k}[/bold green]: {v}")
-		elif k == "age":
+		if k in ["name", "age", "affiliation", "active"]:
 			print(f"[bold green]{k}[/bold green]: {v}")
 		elif k == "items" and isinstance(v, list):
 			print(f"[bold green]{k}[/bold green]")
@@ -29,13 +58,36 @@ def main():
 				if isinstance(item, dict):
 					item_k, item_v = next(iter(item.items()))
 					print(f"  [bold red]{item_k}[/bold red]: {item_v}")
-		elif k == "active":
-			if v:
-				print(f"[bold green]{k}[/bold green]: True")
-			else:
-				print(f"[bold green]{k}[/bold green]: False")
-		elif k == "affiliation":
-			print(f"[bold green]{k}[/bold green]: {v}")
+	print()
+
+	# Displaying a table
+	table = Table("Name", "Item")
+	table.add_row("Frodo", "The Ring of Power")
+	table.add_row("Gandalf", "Staff")
+	table.add_row("Aragorn", "Sword")
+	console.print(table)
+
+	# Printing to the error console
+	err_console.print("There was an error, writing to standard error")
+	print()
+
+	# Printing with bools and styles
+	message_start = "everything is "
+	if good:
+		ending = typer.style("good", fg=typer.colors.GREEN, bold=True)
+	else:
+		ending = typer.style("bad", fg=typer.colors.WHITE, bg=typer.colors.RED)
+	message = message_start + ending
+	typer.echo(message)
+	print()
+
+	# Printing arguments with styles
+	typer.secho(f"Welcome, {dynamic_name}", fg=typer.colors.MAGENTA)
+	print()
+
+	# Printing hidden variables
+	if other_stuff in ["World", "Earth"]:
+		typer.secho(f"Hello, {other_stuff}", fg=typer.colors.RED)
 
 
 if __name__ == "__main__":
